c9288edd7b5d219de125435a3bf41dc3f9e03f1f
Milen Dyankov <milendyankov@gmail.com>
2021-01-07 13:08:07 +0100

    Update SE example to use framework's 4.5 API

--- example/javase/src/main/java/org/axonframework/cdi/example/javase/AccountApplication.java
+++ example/javase/src/main/java/org/axonframework/cdi/example/javase/AccountApplication.java
@@ -8,6 +8,7 @@ import org.axonframework.cdi.example.javase.command.CreateAccountCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventhandling.interceptors.EventLoggingInterceptor;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 
 import java.util.concurrent.TimeUnit;
@@ -27,7 +28,7 @@ public class AccountApplication {
     public void run() {
         eventBus.registerDispatchInterceptor(new EventLoggingInterceptor());
         commandGateway.sendAndWait(new CreateAccountCommand("4711", 1000D));
-        queryGateway.send("4711", Double.class, 1, TimeUnit.SECONDS)
+        queryGateway.scatterGather("4711", ResponseTypes.instanceOf(Double.class), 1, TimeUnit.SECONDS)
                 .forEach(System.out::println);
     }
 
